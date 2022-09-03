6543f174e405770f3fdb4e2d2ddaf5617961b74a
Jarol Saul Espaillat Cruz <89672288+Sweetb720x@users.noreply.github.com>
2022-09-02 21:50:47 -0400

    Primera parte

---
+++ Intro.java
@@ -0,0 +1,10 @@
+public class Intro {
+    public static void main(String[] args){
+        //int resultado = Suma(5,6);
+        System.out.print(Suma(1, 100,200));
+    }
+    public static int Suma(int a, int b, int c){
+        int res = a + b + c;
+        return res;
+    }
+}
